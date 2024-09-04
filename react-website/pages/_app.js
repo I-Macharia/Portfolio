47fc0981eefcd0c5c11bc0ea423491c757450241
“I-Macharia” <gichoya.macharia@gmail.com>
2024-09-04 21:02:09 +0300

    removing react files

--- react-website/pages/_app.js
+++
@@ -1,8 +0,0 @@
-// pages/_app.js
-import '/global.css'; // Adjust the path based on your project structure
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
